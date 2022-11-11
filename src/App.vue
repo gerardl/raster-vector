e5fe5e4e996db7a355f7713df806221797a97bcc
Gerard Lucas <gerard.lucas@conperio.com>
2022-11-11 09:31:41 -0600

    comment cleanup

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@ import Footer from './components/Footer.vue'
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
-
     <div class="wrapper">
       <Title msg="Raster and Vector Images" />
     </div>
@@ -29,22 +27,4 @@ header {
   display: block;
   margin: 0 auto 2rem;
 }
-
-/* @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
